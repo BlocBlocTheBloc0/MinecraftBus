7395dbf245d79c6da4735da145b808c5f38b7a4d
BlocBlocTheBloc <97693441+BlocBlocTheBloc0@users.noreply.github.com>
2023-02-21 01:17:18 +0100

    Update

--- src/main/java/fr/blocblocthebloc/bussounds/init/BussoundsModItems.java
+++ src/main/java/fr/blocblocthebloc/bussounds/init/BussoundsModItems.java
@@ -31,6 +31,36 @@ public class BussoundsModItems {
 	public static final RegistryObject<Item> FEB_29 = block(BussoundsModBlocks.FEB_29, BussoundsModTabs.TAB_SOUNDS);
 	public static final RegistryObject<Item> LITIEUM = block(BussoundsModBlocks.LITIEUM, BussoundsModTabs.TAB_SOUNDS);
 	public static final RegistryObject<Item> FRANK = block(BussoundsModBlocks.FRANK, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> ABBY = block(BussoundsModBlocks.ABBY, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> DECOLOR = block(BussoundsModBlocks.DECOLOR, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> ROLAND = block(BussoundsModBlocks.ROLAND, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> CENTRE_LOGISTIQUE = block(BussoundsModBlocks.CENTRE_LOGISTIQUE, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> ILE = block(BussoundsModBlocks.ILE, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> FEUWAR = block(BussoundsModBlocks.FEUWAR, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> RAILS = block(BussoundsModBlocks.RAILS, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> CIMETIERE = block(BussoundsModBlocks.CIMETIERE, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> RUE = block(BussoundsModBlocks.RUE, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> NOV_11 = block(BussoundsModBlocks.NOV_11, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> CAMPAGNE = block(BussoundsModBlocks.CAMPAGNE, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> FONTAINE = block(BussoundsModBlocks.FONTAINE, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> CHARMAUX = block(BussoundsModBlocks.CHARMAUX, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> PONTDEAU = block(BussoundsModBlocks.PONTDEAU, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> FLAURETE = block(BussoundsModBlocks.FLAURETE, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> DURIAN = block(BussoundsModBlocks.DURIAN, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> TROUBADOUR = block(BussoundsModBlocks.TROUBADOUR, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> ST_POULIN = block(BussoundsModBlocks.ST_POULIN, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> EDUICO = block(BussoundsModBlocks.EDUICO, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> MAIS_1 = block(BussoundsModBlocks.MAIS_1, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> THEATRE = block(BussoundsModBlocks.THEATRE, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> VITICUL = block(BussoundsModBlocks.VITICUL, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> OUTILS = block(BussoundsModBlocks.OUTILS, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> NUANCES = block(BussoundsModBlocks.NUANCES, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> POINT_ROND = block(BussoundsModBlocks.POINT_ROND, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> SANS = block(BussoundsModBlocks.SANS, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> CORPS = block(BussoundsModBlocks.CORPS, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> JUI_14 = block(BussoundsModBlocks.JUI_14, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> LEGUERRIER = block(BussoundsModBlocks.LEGUERRIER, BussoundsModTabs.TAB_SOUNDS);
+	public static final RegistryObject<Item> RESIDANCE = block(BussoundsModBlocks.RESIDANCE, BussoundsModTabs.TAB_SOUNDS);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
